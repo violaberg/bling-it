8452eb061dcbf8610c29802c1cc83ead6b584f89
Viola Bergere <viola.bergere@gmail.com>
2024-04-18 17:57:19 +0000

    Add gemstones template and views

--- gemstones/views.py
+++ gemstones/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Gemstone
 
 # Create your views here.
+def all_gemstones(request):
+    """ A view to return all gemstone page """
+
+    gemstones = Gemstone.objects.all()
+
+    context = {
+        'gemstones': gemstones,
+    }
+
+    return render(request, "gemstones/gemstones.html", context)
