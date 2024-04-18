64c15bcf527cf404bb2e363ed355a9c4d11fad13
Viola Bergere <viola.bergere@gmail.com>
2024-04-18 17:28:13 +0000

    Customize admin panel

--- gemstones/admin.py
+++ gemstones/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Gemstone, Category
 
 # Register your models here.
-admin.site.register(Gemstone)
-admin.site.register(Category)
+class GemstoneAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'cut',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Gemstone, GemstoneAdmin)
+admin.site.register(Category, CategoryAdmin)
--- gemstones/models.py
+++ gemstones/models.py
@@ -63,8 +63,7 @@ class Gemstone (models.Model):
     treatment = models.CharField(max_length=20)
     certification = models.CharField(max_length=20)
     origin = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
