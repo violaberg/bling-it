14f9e92c110fc858c80320fb5eea605f3ad2857a
Viola Bergere <viola.bergere@gmail.com>
2024-04-21 18:13:00 +0000

    Add sorting

--- gemstones/views.py
+++ gemstones/views.py
@@ -10,8 +10,23 @@ def all_gemstones(request):
     gemstones = Gemstone.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                gemstones = gemstones.annotate(lower_name=lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            gemstones = gemstones.order_by(sortkey)
+
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             gemstones = gemstones.filter(category__name__in=categories)
@@ -27,10 +42,13 @@ def all_gemstones(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             gemstones = gemstones.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'gemstones': gemstones,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, "gemstones/gemstones.html", context)
