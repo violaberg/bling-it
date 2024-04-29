5209eabbd844cdc8b5a4be112ce18fa47aa58a6b
Viola Bergere <viola.bergere@gmail.com>
2024-04-29 19:09:57 +0000

    Add toasts and style them

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib import messages
+from gemstones.models import Gemstone
 
 # Create your views here.
 def view_bag(request):
@@ -12,11 +13,13 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a specified product to the shopping bag """
 
+    gemstone = Gemstone.objects.get(pk=item_id)
     quantity = 1
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     bag[item_id] = quantity
+    messages.success(request, f'Added {gemstone.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
