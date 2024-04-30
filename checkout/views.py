394ead4d9545879024d2a06a9f3678dda27602cf
Viola Bergere <viola.bergere@gmail.com>
2024-04-30 20:04:42 +0000

    Install crispy forms,create checkout template and css

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
+
 
 # Create your views here.
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, 'Your bag is empty at the moment!')
+        return redirect(reverse('gemstones'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render (request, template, context)
