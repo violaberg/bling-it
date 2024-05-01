8241cb11a0f662f49d7310106e6775358dd215b6
Viola Bergere <viola.bergere@gmail.com>
2024-05-01 11:39:59 +0000

    Format html templates, style checkout page

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import OrderLineItem
+from .models import Order, OrderLineItem, OrderStatus
 
 
 @receiver(post_save, sender=OrderLineItem)
