4e1ffdb67325cd856455553130ba8b62d034c925
Viola Bergere <viola.bergere@gmail.com>
2024-05-08 20:06:30 +0000

    Create about, faq, privacy policy views and add urls

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('', views.about, name='about'),
+    path('', views.faq, name='faq'),
+    path('', views.privacy_policy, name='privacy_policy'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,21 @@ def index(request):
     """ A view to return the index page"""
 
     return render(request, "home/index.html")
+
+
+def about(request):
+    """ A view to return the About Us page"""
+
+    return render(request, "home/about_us.html")
+
+
+def faq(request):
+    """ A view to return the FAQ page"""
+
+    return render(request, "home/faq.html")
+
+
+def privacy_policy(request):
+    """ A view to return the Privacy Policy page"""
+
+    return render(request, "home/privacy_policy.html")
