310f230eaa73e61d8888c582a19b13483d70cbac
Viola Bergere <viola.bergere@gmail.com>
2024-05-09 16:49:04 +0000

    Add contact view, model, form and templates

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('contact_success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
