b06984ac82c44ed03efc0824f5148eeae8f3ad1b
Viola Bergere <viola.bergere@gmail.com>
2024-05-15 17:50:22 +0000

    Fix the image field

--- gemstones/forms.py
+++ gemstones/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Gemstone, Category
 
 
@@ -7,6 +8,8 @@ class GemstoneForm(forms.ModelForm):
     class Meta:
         model = Gemstone
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
