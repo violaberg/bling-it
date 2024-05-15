b43991d0fde616dc0e14eb5dbbba526f4c5eea93
Viola Bergere <viola.bergere@gmail.com>
2024-05-15 18:40:02 +0000

    Remove secret key and set debug

--- bling_it/settings.py
+++ bling_it/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#+5-kzhsjm_c_to0349up7lfbwkp&boahjha@o62-!tm7v$!*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bling-it-242ac659e1b6.herokuapp.com', 'localhost']
 
