5c7776719c0faadfb74421c7c8549fc05c93bd5a
Viola Bergere <viola.bergere@gmail.com>
2024-05-15 10:19:28 +0000

    Create gemstone form for admin

--- bling_it/settings.py
+++ bling_it/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)#+5-kzhsjm_c_to0349up7lfbwkp&boahjha@o62-!tm7v$!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
