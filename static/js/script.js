65cf9a08d3b56f8f8188f6ced5844f3b2c0760f0
Viola Bergere <viola.bergere@gmail.com>
2024-06-05 10:37:36 +0000

    Update review modal js and style it

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var addToWishlistIcon = document.getElementById("add-to-wishlist");
-    var gemstoneId = addToWishlistIcon.getAttribute("data-gemstone-id");
-
-    addToWishlistIcon.addEventListener("click", function() {
-        addToWishlist(gemstoneId);
-    });
-
     // Get the modal and delete button elements
     var deleteReviewLink = document.getElementById("deleteReviewLink");
     var deleteModal = document.getElementById("myModal");
     var cancelDeleteBtn = document.getElementById("cancelDeleteBtn");
+    var confirmDeleteBtn = document.getElementById("confirmDeleteBtn");
 
-    // Add event listeners to open and close the modal
+    // Add event listeners for the modal
     deleteReviewLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default behavior of the link
-        deleteModal.style.display = "block"; // Show the modal
+        event.preventDefault();
+        deleteModal.style.display = "block";
     });
 
     cancelDeleteBtn.addEventListener("click", function() {
-        deleteModal.style.display = "none"; // Hide the modal
+        deleteModal.style.display = "none";
+    });
+
+    confirmDeleteBtn.addEventListener("click", function() {
+        deleteModal.style.display = "none";
     });
 });
-
-function addToWishlist(gemstoneId) {
-    var csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-    var url = "profiles/add_to_wishlist/";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("X-CSRFToken", csrfToken);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Handle success response
-            alert("Gemstone added to wishlist!");
-        } else {
-            // Handle error response
-            alert("Failed to add gemstone to wishlist!");
-        }
-    };
-    xhr.send(JSON.stringify({ gemstone_id: gemstoneId }));
-}
