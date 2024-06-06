be4b406649697134bbf09ec8bfee49568bf670b2
Viola Bergere <viola.bergere@gmail.com>
2024-06-06 16:42:12 +0000

    Install whitenoise

--- bling_it/settings.py
+++ bling_it/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-violaberg-blingit-8ylrt9lbn7x.ws.codeinstitute-ide.net',
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
